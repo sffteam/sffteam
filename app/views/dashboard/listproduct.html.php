eb27f76b567a7eae3c885758319743d601831ee0
Nilam Doctor <nilamdoc@gmail.com>
2019-02-01 20:20:47 +0530

    images

--- app/views/dashboard/listproduct.html.php
+++ app/views/dashboard/listproduct.html.php
@@ -6,7 +6,7 @@ foreach($products as $p){?>
 <div class="row" style="border-bottom:1px solid gray">
 <div class="col-md-1"><small><?=$i?>-<?=$p['code']?> <?php if($code==$p['code']){echo "<b>XXX</b>";}?></small></div>
 <div class="col-md-2"><?=$p['category']?></div>
-<div class="col-md-5"><?=$p['name']?></div>
+<div class="col-md-5"><?=$p['name']?><img src="/img/products/<?=$p['code']?>.jpg" width="30"></div>
 <div class="col-md-1"><?=$p['mrp']?></div>
 <div class="col-md-1"><?=$p['dp']?></div>
 <div class="col-md-1"><?=$p['bv']?></div>
