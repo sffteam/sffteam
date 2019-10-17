ea1905a15a4d0cae022d998e9eb1f0e19af24828
Nilam Doctor <nilamdoc@gmail.com>
2019-10-18 01:47:37 +0530

    sff new

---
+++ app/views/category/index.html.php
@@ -0,0 +1,17 @@
+<p>&nbsp;</p>
+<div class="container">
+<div class="row">
+	<div class="col-2">
+		<h4>Categories</h4>
+		<ul class="list-unstyled">
+			<?php foreach($CategoriesArray as $k => $v){?>
+			<li><a class="text-muted" href="/category/i/<?=$k?>"><?=$v?></a></li>
+			<?php }?>
+		</ul>
+	</div>
+	<div class="col-6">
+		<div class="row"><h4>Products</h4>
+	</div>
+</div>
+</div>
+</div>
