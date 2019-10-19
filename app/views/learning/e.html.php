870bc87fffc07a8501b9744ebe4390f6c3c06f77
Nilam Doctor <nilamdoc@gmail.com>
2019-10-19 18:35:24 +0530

    amp

--- app/views/learning/e.html.php
+++ app/views/learning/e.html.php
@@ -14,9 +14,15 @@ $Learning = array(
 ?>
 <div class="container">
 <div class="mbr-pb-4">
-	
+		
 	<h4>Fill in the form to download</h4>
-	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBKG3f5TFhfvTjaaWNd_ks0mGkGyMyUdfaTeA76Dv1ZA5ocA/viewform?embedded=true" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+	<amp-iframe 
+	 sandbox="allow-scripts allow-same-origin"
+		src="https://docs.google.com/forms/d/e/1FAIpQLSeBKG3f5TFhfvTjaaWNd_ks0mGkGyMyUdfaTeA76Dv1ZA5ocA/viewform?embedded=true" 
+		width="600" 
+		height="600" 
+		frameborder="0" 
+		>Loading…</iframe>
 	</div>
 	
 	<div class="mbr-pb-4">
@@ -24,7 +30,7 @@ $Learning = array(
 		<ul class="list-unstyled">
 			<?php foreach($Learning as $k => $v){?>
 			<li><a class="text-muted" href="/learning/e/<?=$k?>"><?=$v?><br>
-			<img src="/img/documents/Images/<?=str_replace(" ","%20",$v);?>_Page_01.jpg" width="200">
+			<amp-img src="/img/documents/Images/<?=str_replace(" ","%20",$v);?>_Page_01.jpg" width="100" height="100" />
 			
 			</a>
 			
