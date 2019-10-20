035143aaf0b900f507368cec5aef73c36a338c54
Nilam Doctor <nilamdoc@gmail.com>
2019-10-20 16:05:10 +0530

    site verification

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -7,6 +7,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="google-site-verification" content="JAgACjMg-4trWd6Kg7nSN8LSgB3qcpGv2W0GQCwWN64" />
   <meta name="generator" content="<?=COMPANY_URL?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
   <link rel="shortcut icon" href="<?php echo 'https://' . $_SERVER['SERVER_NAME']. '/img/logo.png';?>" type="image/x-icon">
