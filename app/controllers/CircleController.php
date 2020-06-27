065fc1f3160dba1d59103765b79a4cf5966e6e5e
Nilam Doctor <nilamdoc@gmail.com>
2020-06-27 14:16:36 +0530

    Circle -> Assets

--- app/controllers/CircleController.php
+++ app/controllers/CircleController.php
@@ -108,8 +108,66 @@ class CircleController extends \lithium\action\Controller {
 				}
 				
 			}
-			return $this->render(array('json' => array("success"=>"Yes",'products'=>$data)));		
+			return $this->render(array('json' => array("success"=>"Yes",'categories'=>$data)));		
 		
+	}
+
+	public function addassets(){
+		
+		$products = Malls::find('all',array());
+			$i = 1;
+			
+		foreach($products as $p){
+			$category = substr($p['Code'],0,2);
+				$find = X_categories::find('first',array(
+						'conditions'=>array('alias'=>$category)
+				));
+			
+			$data = array(
+			 "id"=>$i,
+				"category_id"=>$find->id,
+				"is_published"=>1,
+				"date_added"=> gmdate('Y-m-d h:i:s'),
+				"created_by"=>1,
+				"created_by_user"=>'Nilam Doctor',
+				"date_modified"=>gmdate('Y-m-d h:i:s'),
+				"modified_by"=>1,
+				"modified_by_user"=>'Nilam Doctor',
+				"checked_out"=>null,
+				"checked_out_by"=>null,
+				"checked_out_by_user"=>'Nilam Doctor',
+				"title"=>$p->Name,  
+				"description"=>'MRP: '. number_format((float)$p->MRP, 2, '.', '') .', DP: '.number_format((float)$p->DP, 2, '.', '').', BV: '.number_format((float)$p->BV, 2, '.', '').', PV: '.number_format((float)$p->PV, 2, '.', ''),
+				"alias"=>$p->Code,
+				"storage_location"=>'remote',
+				"path"=>null,
+				"remote_path"=>"https://sff.team/img/products/".$p->Code."_400.jpg",
+				"original_file_name"=>$p->Code,
+				"lang"=>'en',
+				"publish_up"=>null,
+				"publish_down"=>null,
+				"download_count"=>0,
+				"unique_download_count"=>0,
+				"revision"=>0,
+				"extension"=>'jpg',
+				"mime"=>'image/jpeg',
+				"size"=>0,
+				"disallow"=>0
+			);
+			$i++;
+
+				$find = X_assets::find('first',array(
+						'conditions'=>array('original_file_name'=>$p->Code)
+				));
+				$conditions = array('original_file_name'=>$p->Code);
+				if(count($find)>0){
+					X_assets::update($data,$conditions);
+					
+				}else{
+					X_assets::create()->save($data);
+				}
+		}
+		return $this->render(array('json' => array("success"=>"Yes",'categories'=>$data)));		
 	}
 
 
