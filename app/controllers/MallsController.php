4698eb536821f5fa06c433dbce3ca7ccfea7f541
Nilam Doctor <nilamdoc@gmail.com>
2020-09-15 09:37:01 +0530

    QDLegs

--- app/controllers/MallsController.php
+++ app/controllers/MallsController.php
@@ -2428,6 +2428,8 @@ public function getbuilders(){
       'Region'=>$n['Zone'].'-'.$n['City']?:"",
       'Mobile'=>$mobile['Mobile']?:"",
       'Level'=>$n[$yyyymm]['Level']?:"",
+      'Legs'=>$n[$yyyymm]['Legs']?:0,
+      'QDLegs'=>$n[$yyyymm]['QDLegs']?:0,
      )
     );
    }
