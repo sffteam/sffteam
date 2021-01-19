86e0458fb983f6288fea2ef1661dd85f70137e63
Nilam Doctor <nilamdoc@gmail.com>
2021-01-19 12:19:39 +0530

    ok

--- app/views/m2l21/index.html.php
+++ app/views/m2l21/index.html.php
@@ -5,11 +5,11 @@ $i = 1;
 foreach($participants as $p){
  
  ?>
-<div class="row Share sz1">
-<div class="col"><?=$i?></div>
- <div class="col"><?=$p['mcaNumber']?></div>
- <div class="col"><?=$p['Name']?></div>
- <div class="col"><?php
+<div class="row Share sz1" style="border-bottom:1px solid">
+ <div class="col-10" style="border-right:1px solid"><?=$i?></div>
+ <div class="col-20" style="border-right:1px solid"><?=$p['mcaNumber']?></div>
+ <div class="col-40" style="border-right:1px solid"><?=$p['Name']?></div>
+ <div class="col-20" style="border-right:1px solid"><?php
  foreach($mobiles as $m){
   if($p['mcaNumber']===$m['mcaNumber']){
    $mobile =  $m['Mobile'];
@@ -18,7 +18,7 @@ foreach($participants as $p){
  }
  echo '<a class="link external" target="_blank" href="https://wa.me/?91'.trim($mobile).'?text=Mission%202L21%20Registration%20Link:%20https://sffteam/m2l21/event/'.$p['mcaNumber'].'">'.$mobile.'</a>';
  ?></div>
- <div class="col"><?=$p['Payment']?></div>
+ <div class="col-10" style="border-right:1px solid"><?=$p['Payment']?></div>
 </div>
 <?php 
 $i++;
