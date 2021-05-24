3fe919c4fe07204d92897a225467a74335ddadbc
Nilam Doctor <nilamdoc@gmail.com>
2021-05-24 16:35:20 +0530

    Web App

---
+++ app/views/elements/mca_header.html.php
@@ -0,0 +1,15 @@
+
+  <!-- Fixed navbar goes ALWAYS FIRST -->
+  <div class="navbar sz1 text-align-center " style="background-color:#ff0000;padding-top:5px;">
+   <h1>WeCapacitate</h1>
+  </div>
+  <!-- Fixed toolbar goes ALWAYS AFTER navbar -->
+  <div class="toolbar toolbar-bottom">
+    <div class="toolbar-inner">
+          <a href="/mca/" class="external link">Home</a>
+          <a href="/mca/reports/" class="external link">Reports</a>
+          <a href="/mca/pdfs/" class="external link">PDFs</a>
+          <a href="/mca/videos/" class="external link">Videos</a>
+        </div>
+  </div>
+
---
+++ app/views/layouts/mca.html.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
+  <meta http-equiv="Content-Security-Policy" content="default-src * data: gap: content: https://ssl.gstatic.com; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400&family=Raleway:wght@100;300;400&family=Share&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/framework7/css/framework7.bundle.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/app.css">
+  <script src="/framework7/js/framework7.bundle.min.js"></script>
+  <script src="/js/mcaapp.js"></script>
+
+  </head>
+
+<body>
+		<div class="page">
+  <h2 class="Poppins text-align-center sz1">WeCapacitate</h2>
+   <?php echo $this->_render('element', 'mca_header');?>	
+   <div class="page-content">
+			<?php echo $this->content(); ?>
+   </div>
+   </div>
+  </div>
+</body>
+<!-- JS code -->
+ <script src="/js/mcaroutes.js"></script>
+</html>
