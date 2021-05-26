c3cd00d94ef171597a84855ef5ca1cd2f6c99b16
Nilam Doctor <nilamdoc@gmail.com>
2021-05-26 17:49:34 +0530

    app changes

--- app/controllers/McaController.php
+++ app/controllers/McaController.php
@@ -16,21 +16,44 @@ protected function _init() {
   $this->_render['layout'] = 'mca';
  }
  public function index(){
+  $url = 'index';
+  return compact('url');
  }
  public function reports(){
+  $url = 'reports';
+  return compact('url');
+ }
+ 
+ public function products(){
+  $url = 'products';
+  return compact('url');
  }
  public function pdfs(){
+  $url = 'pdfs';
+  return compact('url');
  }
  public function videos(){
+  $url = 'videos';
+  return compact('url');
  }
  public function discovery(){
+  $url = 'videos';
+  return compact('url');
  }
  public function business(){
+  $url = 'videos';
+  return compact('url');
  }
  public function productvideo(){
+  $url = 'videos';
+  return compact('url');
  }
  public function objection(){
+  $url = 'videos';
+  return compact('url');
  }
  public function knowledge(){
+  $url = 'videos';
+  return compact('url');
  } 
 }
