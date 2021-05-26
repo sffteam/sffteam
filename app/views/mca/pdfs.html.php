f1d64324129e7066524add4c3d5e1fd7865a0506
Nilam Doctor <nilamdoc@gmail.com>
2021-05-26 22:06:33 +0530

    PDFs

---
+++ app/views/mca/pdfs.html.php
@@ -0,0 +1,15 @@
+<div class="navbar">
+  <div class="navbar-bg"></div>
+  <div class="navbar-inner sliding">
+    <div class="title">Download PDFs</div>
+  </div>
+</div>
+<div class="block-title">Discovery</div>
+      <div class="list links-list">
+        <ul>
+          <li><a href="/img/documents/WeCapacitate.Brochure-Modicare.pdf" class="external link" target="_blank">Discovery Preview</a></li>
+          <li><a href="/img/documents/Shuruaat for LMS.pdf" class="external link" target="_blank">Shuruat</a></li>
+          <li><a href="/img/documents/Raftaar.pdf" class="external link" target="_blank">Raftaar</a></li>
+          <li><a href="/img/documents/Parivartan_SMAPUnlimited.pdf" class="external link" target="_blank">Parivartan</a></li>
+        </ul>
+      </div>
