70714841b9a1fbb793cdf5c427cdb401305e355f
Nilam Doctor <nilamdoc@gmail.com>
2021-05-28 09:04:47 +0530

    all

---
+++ app/views/mca/index.html.php
@@ -0,0 +1,10 @@
+<div class="navbar">
+  <div class="navbar-bg bg-color-black"></div>
+  <div class="navbar-inner sliding">
+    <a href="/mca/" class="external link text-color-white">Home</a>
+    <a href="/mca/reports/" class="external link text-color-white">Reports</a>
+    <a href="/mca/pdfs/" class="external link text-color-white">PDFs</a>
+    <a href="/mca/products/categories/" class="external link text-color-white">Products</a>
+    <a href="/mca/videos/" class="external link text-color-white">Videos</a>
+  </div>
+</div>
--- app/views/mca/objection.html.php
+++ app/views/mca/objection.html.php
@@ -1,7 +1,11 @@
 <div class="navbar">
-  <div class="navbar-bg"></div>
+  <div class="navbar-bg bg-color-black"></div>
   <div class="navbar-inner sliding">
-    <div class="title">Objection Handling</div>
+    <a href="/mca/" class="external link text-color-white">Home</a>
+    <a href="/mca/reports/" class="external link text-color-white">Reports</a>
+    <a href="/mca/pdfs/" class="external link text-color-white">PDFs</a>
+    <a href="/mca/products/categories/" class="external link text-color-white">Products</a>
+    <a href="/mca/videos/" class="external link text-color-white">Videos</a>
   </div>
 </div>
 
