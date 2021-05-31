c4f4e5b0e3d042113078f70569b5e87e9c136297
Nilam Doctor <nilamdoc@gmail.com>
2021-05-31 11:47:01 +0530

    mca

--- app/controllers/McaController.php
+++ app/controllers/McaController.php
@@ -58,12 +58,139 @@ protected function _init() {
      ),
    'order'=>array()
    ));
-
-  
+   
+  $allusers = $this->searchdown($mcaNumber);
   
   $url = 'reports';
-  return compact('url','mcaNumber','user','mcaDetails','findmobile','team','joinee','users');
+  return compact('url','mcaNumber','user','mcaDetails','findmobile','team','joinee','users','allusers');
  }
+
+
+public function searchdown($mcaNumber=null){
+ ini_set('memory_limit', '-1');
+ $yyyymm = date('Y-m');
+ $pyyyymm = date('Y-m', strtotime('first day of last month'));
+ 
+ if($mcaNumber){
+  $users = Users::find('all',array(
+   'conditions'=>array(
+    'refer'=>$mcaNumber,
+     $yyyymm=>array('$exists'=>true),
+     $pyyyymm.'.Percent'=>array('$ne'=>null)
+     ),
+   'order'=>array($yyyymm.'.GPV'=>'DESC',$pyyyymm.'.GPV'=>'DESC')
+   ));
+
+  if(count($users)>0){
+   $allusers = array();
+   foreach($users as $u){
+    $count = Users::count(array(
+     'conditions'=>array('refer'=>$u['mcaNumber'])
+    ));
+    $mobile = Mobiles::find('first',array(
+     'conditions'=>array('mcaNumber'=>$u['mcaNumber'])
+    ));
+    $yyyymm = date('Y-m');
+    $pyyyymm = date('Y-m', strtotime('first day of last month'));
+    
+    $joinee = $this->findJoinee($u['mcaNumber']);
+    
+    $joineePV = 0;
+    foreach($joinee as $j){
+     $joineePV = $joineePV + $j['PV'];
+    }
+//    if($user['Enable']=='Yes'){
+  //  }
+  
+  $Indusers = Users::find('first',array(
+   'conditions'=>array(
+    'mcaNumber'=>$u['mcaNumber'],
+     $yyyymm=>array('$exists'=>true),
+     $pyyyymm.'.Percent'=>array('$ne'=>null)
+     ),
+   'order'=>array($yyyymm.'.GPV'=>'DESC',$pyyyymm.'.GPV'=>'DESC')
+   ));
+//   print_r($Indusers['left']);
+   $team = Users::count('all',array('conditions'=>
+   array(
+      'left'=>array('$gt'=>$Indusers['left']),
+      'right'=>array('$lt'=>$Indusers['right']),
+//       $yyyymm=>array('$exists'=>true),
+//       $p1yyyymm.'.Percent'=>array('$ne'=>null),
+      'Enable'=>'Yes'
+   )
+   )
+ );
+ 
+    array_push($allusers,array(
+    'mcaNumber'=>$u['mcaNumber'],
+    'mcaName'=>$u['mcaName'],
+    'refer'=>$u['refer'],
+    'Mobile'=>$mobile['Mobile']?:"",
+    'Enable'=>$u['Enable'],
+    'Level'=>$u['Level'],
+    'KYC'=>$u['KYC']?:"",
+    'NEFT'=>$u['NEFT']?:"",
+    'DateJoin'=>$u['DateJoin'],
+    'State'=>$u['State'],
+    'Zone'=>$u['Zone'],
+    'City'=>$u['City'],
+     $yyyymm=>array(
+     'PV'=>$u[$yyyymm]['PV']?:0,
+     'ExtraPV'=>$u[$yyyymm]['ExtraPV']?:0,
+     'BV'=>$u[$yyyymm]['BV']?:0,
+     'GBV'=>$u[$yyyymm]['GBV']?:0,
+     'GPV'=>$u[$yyyymm]['GPV']?:0,
+     'GrossPV'=>$u[$yyyymm]['GrossPV']?:0,
+     'PGPV'=>$u[$yyyymm]['PGPV']?:0,
+     'PGBV'=>$u[$yyyymm]['PGBV']?:0,
+     'RollUpBV'=>$u[$yyyymm]['RollUpBV']?:0,
+     'RollUpPV'=>$u[$yyyymm]['RollUpPV']?:0,
+     'Legs'=>$u[$yyyymm]['Legs']?:0,
+     'QDLegs'=>$u[$yyyymm]['QDLegs']?:0,
+     'Percent'=>$u[$yyyymm]['Percent']?:"",
+     'ValidTitle'=>$u[$yyyymm]['ValidTitle']?:"",
+     'Joinee'=>count($joinee),
+     'Team'=>$team,
+     'InActive' => (integer)$u[$yyyymm]['InActive']?:"",
+    ),
+    $pyyyymm => array(
+     'PV'=>$u[$pyyyymm]['PV']?:0,
+     'ExtraPV'=>$u[$pyyyymm]['ExtraPV']?:0,
+     'BV'=>$u[$pyyyymm]['BV']?:0,
+     'GBV'=>$u[$pyyyymm]['GBV']?:0,
+     'GPV'=>$u[$pyyyymm]['GPV']?:0,
+     'GrossPV'=>$u[$pyyyymm]['GrossPV']?:0,
+     'PGPV'=>$u[$pyyyymm]['PGPV']?:0,
+     'PGBV'=>$u[$pyyyymm]['PGBV']?:0,
+     'RollUpBV'=>$u[$pyyyymm]['RollUpBV']?:0,
+     'RollUpPV'=>$u[$pyyyymm]['RollUpPV']?:0,
+     'Legs'=>$u[$pyyyymm]['Legs']?:0,
+     'QDLegs'=>$u[$pyyyymm]['QDLegs']?:0,
+     'Percent'=>$u[$pyyyymm]['Percent']?:0,
+     'ValidTitle'=>$u[$pyyyymm]['ValidTitle']?:"",
+     'InActive' => (integer)$u[$pyyyymm]['InActive']?:0,
+     'Gross' => $u[$pyyyymm]['Gross']?:0,     
+     'APB' => $u[$pyyyymm]['APB']?:0,     
+     'TBB' => $u[$pyyyymm]['TBB']?:0,     
+     'ABB' => $u[$pyyyymm]['ABB']?:0,     
+     'DB' => $u[$pyyyymm]['DB']?:0,     
+     'LPB' => $u[$pyyyymm]['LPB']?:0,     
+     'TF' => $u[$pyyyymm]['TF']?:0,     
+     'CF' => $u[$pyyyymm]['CF']?:0,     
+     'HF' => $u[$pyyyymm]['HF']?:0,     
+     ),
+    'count'=>$count
+    ));
+   }
+   
+   return $allusers;
+  }else{
+   return null;  
+  }
+ }
+ return null;
+}
  
  public function products($Code=null){
   $Categories = array(
--- app/views/mca/reports.html.php
+++ app/views/mca/reports.html.php
@@ -8,6 +8,7 @@
     <a href="/mca/videos/" class="external link text-color-white">Videos</a>
   </div>
 </div>
+
 <?php
 $yyyymm = date("Y-m", strtotime("0 month", strtotime(date("F") . "1")) );
 if($mcaNumber==null){
@@ -19,35 +20,57 @@ if($mcaNumber==null){
  
       <div class="card card-expandable ">
         <div class="card-content ">
-          <div class="bg-color-white" style="height: 300px;background-color:#ff0000">
-            <div class="card-header text-color-black display-block"><?=$mcaDetails['mcaName']?><br />
-              <small style="opacity: 0.8"><?=$mcaDetails['mcaNumber']?>    </small><br>
+          <div class="bg-color-white" style="height: 300px">
+            <div class="card-header text-color-black display-block"><small><?=$mcaDetails['mcaName']?></small><br />
+              <small style="opacity: 0.8"><?=$mcaDetails['mcaNumber']?>   </small><br>
               <small class="sz16 Bebas" style="opacity: 0.7"><?=$mcaDetails[$yyyymm]['ValidTitle']?> - <?=$mcaDetails['DateJoin']?></small><br>
-              <small style="opacity: 0.7">PV: <?=$mcaDetails[$yyyymm]['PV']?> - GPV: <?=$mcaDetails[$yyyymm]['GPV']?></small><br>
-              <small style="opacity: 0.7">PGPV: <?=$mcaDetails[$yyyymm]['PGPV']?> - Rollup: <?=$mcaDetails[$yyyymm]['RollUpPV']?></small><br>
-              <small style="opacity: 0.7">BV: <?=$mcaDetails[$yyyymm]['BV']?> - GBV: <?=$mcaDetails[$yyyymm]['GBV']?></small><br>
-              <small style="opacity: 0.7">Joining: <?=$joinee?> - Team: <?=$team?></small><br>
+              <small class="sz16" style="opacity: 0.7">PV: <?=$mcaDetails[$yyyymm]['PV']?> - EPV: <?=$mcaDetails[$yyyymm]['ExtraPV']?> - GPV: <?=$mcaDetails[$yyyymm]['GPV']?></small><br>
+              <small class="sz16" style="opacity: 0.7">PGPV: <?=$mcaDetails[$yyyymm]['PGPV']?> - Rollup: <?=$mcaDetails[$yyyymm]['RollUpPV']?></small><br>
+              <small class="sz16" style="opacity: 0.7">BV: <?=$mcaDetails[$yyyymm]['BV']?> - GBV: <?=$mcaDetails[$yyyymm]['GBV']?></small><br>
+              <small class="sz16" style="opacity: 0.7">Joining: <?=$joinee?> - Team: <?=$team?></small><br>
             </div>
             <a href="#" class="link card-close card-opened-fade-in color-red"
               style="position: absolute; right: 15px; top: 15px"><strong>X</strong>
               <i class="icon f7-icons">xmark</i>
             </a>
-          </div>
+   
           <div class="card-content-padding">
-              <i class="icons f7-icons">phone</i> <a href="tel:<?=$findmobile['Mobile']?>" class="link external"><?=$findmobile['Mobile']?></a>
-              <?php
-              foreach($users as $u){
-               print_r($u['mcaName']);
-               
-              }
-               ;
-              ?>
+            <i class="icons f7-icons">phone</i><a href="tel:<?=$findmobile['Mobile']?>" class="link external"><?=$findmobile['Mobile']?></a>
+            <a href="/mca/reports/<?=$mcaDetails['refer']?>" class="external link">Previous</a>
+          </div>
           </div>
         </div>
       </div>
-    
- 
- 
+                  <?php
+               foreach($allusers as $u){
+               ?>
+<div class="card card-expandable ">
+ <div class="card-content ">
+  <div class="bg-color-red" style="height: 300px;background-color:#ff0000">
+   <div class="card-header text-color-black display-block"><?=$u['mcaName']?><br />
+   <small style="opacity: 0.8"><?=$u['mcaNumber']?>    </small><br>
+   <small class="sz18 Bebas" style="opacity: 0.7"><?=$u[$yyyymm]['ValidTitle']?> - <?=$u['DateJoin']?></small><br>
+   <small class="sz16" style="opacity: 0.7">PV: <?=$u[$yyyymm]['PV']?> - EPV: <?=$u[$yyyymm]['ExtraPV']?> - GPV: <?=$u[$yyyymm]['GPV']?></small><br>
+   <small class="sz16" style="opacity: 0.7">PGPV: <?=$u[$yyyymm]['PGPV']?> - Rollup: <?=$u[$yyyymm]['RollUpPV']?></small><br>
+   <small class="sz16" style="opacity: 0.7">BV: <?=$u[$yyyymm]['BV']?> - GBV: <?=$u[$yyyymm]['GBV']?></small><br>
+   <small class="sz16" style="opacity: 0.7">Joining: <?=$u[$yyyymm]['Joinee']?> - Team: <?=$u[$yyyymm]['Team']?></small><br>
+   </div>
+   <a href="#" class="link card-close card-opened-fade-in color-red"
+   style="position: absolute; right: 15px; top: 15px"><strong>X</strong>
+   <i class="icon f7-icons">xmark</i>
+   </a>
+   </div>
+  <div class="card-content-padding">
+  <i class="icons f7-icons">phone</i> <a href="tel:<?=$u['Mobile']?>" class="link external"><?=$u['Mobile']?></a>
+  <a href="/mca/reports/<?=$u['mcaNumber']?>" class="external link">Next Team</a>
+  </div>
+ </div>
+</div>
+               
+   <?php
+                }
+              ?>
+
  <?php
 }
 ?>
