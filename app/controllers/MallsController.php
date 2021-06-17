8fe81c13a702560585d9136d6f43a71cd867c4bd
Nilam Doctor <nilamdoc@gmail.com>
2021-06-17 12:44:15 +0530

    Active

--- app/controllers/MallsController.php
+++ app/controllers/MallsController.php
@@ -5881,6 +5881,52 @@ $yyyymm = date("Y-m", strtotime("0 month", strtotime(date("F") . "1")) );
  return compact('todayJoining');
 }
 
+public function active(){
+set_time_limit(0);
+ini_set('memory_limit','-1'); 
+$yyyymm = date("Y-m", strtotime("0 month", strtotime(date("F") . "1")) );
+
+ $users = Users::find('all',array(
+  'conditions'=>array($yyyymm.'.PV'=>array('$gt'=>0),'Enable'=>'Yes')
+ ));
+ $todayJoining = array();
+ foreach($users as $u){
+  $mcaNumber = $u['mcaNumber'];
+   $findmobile = Mobiles::find('first',array(
+   'conditions'=>array('mcaNumber'=>$mcaNumber,)
+   ));
+   $findrefermobile = Mobiles::find('first',array(
+   'conditions'=>array('mcaNumber'=>$u['refer'])
+   ));
+   $team = Users::find('all',array(
+    'conditions'=>array('refer'=>$u['mcaNumber'])
+   ));
+   $allteam = " ";
+   foreach($team as $t){
+    $findTeammobile = Mobiles::find('first',array(
+   'conditions'=>array('mcaNumber'=>$t['mcaNumber'])
+   ));
+    $allteam = $allteam . "\\n".$t['mcaName']." *GPV: ".$t[$yyyymm]['GPV']." PGPV: ".$t[$yyyymm]['PGPV']."* (+91".$findTeammobile['Mobile']."), ";
+    
+   }
+
+
+   if($findmobile['Mobile']){
+    if($findrefermobile['Mobile']){
+     array_push($todayJoining,array(
+      'name'=>$u['mcaName']." (MCA No: ".$u['mcaNumber'].")",
+      'Mobile'=>$findmobile['Mobile'],
+      'refer'=>$u['refer_name']." (+91".$findrefermobile['Mobile'].")",
+      'KYCNEFT'=>"KYC: ". $u['KYC'].", NEFT Approved: ".$u['NEFT'],
+      'DateJoin'=>$u['DateJoin'],
+      'Info'=>"Your Valid Title is: *".$u[$yyyymm]['ValidTitle']."* This month you have done ".$u[$yyyymm]['PV']." PV & ".$u[$yyyymm]['ExtraPV']." Extra PV, your team total *". $u[$yyyymm]['GPV']." GPV, & ". $u[$yyyymm]['PGPV']." PGPV*.\\n Every month you and your team require 1250 GPV or 1250 Pure PGPV. Once you qualify by doing this, you will start earning from Modicare.\\n Your Team: " . $allteam,
+    ));
+    }
+   }
+ }
+ 
+ return compact('todayJoining');
+}
 
 public function addEPV(){
 set_time_limit(0);
