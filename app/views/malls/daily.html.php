cbc5d2df01899039da762ced9a8ff3132cc16484
Nilam Doctor <nilamdoc@gmail.com>
2021-07-18 09:49:35 +0530

    up

--- app/views/malls/daily.html.php
+++ app/views/malls/daily.html.php
@@ -5,11 +5,11 @@
  $p1yyyymmdays=cal_days_in_month(CAL_GREGORIAN,date('m', strtotime("-1 month", strtotime(date("F") . "1"))),date('Y', strtotime("-1 month", strtotime(date("F") . "1"))));
  $totalDays = $yyyymmdays + $p1yyyymmdays;
 ?>
-<h1 class="Raleway sz1"><strong><?=$self['mcaName']?> - (<a href="/tree/index/<?=$self['mcaNumber']?>/<?=$yyyymm?>/d" class="external links" title="Open Tree Structure" target="_blank"><?=$self['mcaNumber']?></a>) <?=$self[$yyyymm]['PaidTitle']?> - <?=$self['DateJoin']?> <br><small>KYC: <?=$self['KYC']?>, NEFT: <?=$self['NEFT']?>  <a href="/malls/snapshot/<?=$self['mcaNumber']?>" class="external link">Snapshot</a> - <a href="/malls/daily/<?=$self['mcaNumber']?>" class="external link">Daily</a></small></strong></h1>
+<h1 class="Raleway sz1"><strong><?=$self['mcaName']?> - (<a href="/tree/index/<?=$self['mcaNumber']?>/<?=$yyyymm?>/d" class="external links" title="Open Tree Structure" target="_blank"><?=$self['mcaNumber']?></a>) <?=$self[$yyyymm]['PaidTitle']?> - <?=$self['DateJoin']?> <br><small>KYC: <?=$self['KYC']?>, NEFT: <?=$self['NEFT']?>  <a href="/malls/snapshot/<?=$self['mcaNumber']?>" class="external link">Snapshot</a> - <a href="/malls/daily/<?=$self['mcaNumber']?>" class="external link">Daily</a></small> Daily GPV report</strong></h1>
 <table cellspacing=0 cellpadding=0 class="Roboto">
  <tr>
  <th class="right left">Name</th>
- <?php for($i=40;$i>=0;$i--){
+ <?php for($i=0;$i<=31;$i++){
   $prevDate = date("M-d", strtotime('today - '.$i.' days') );
   $yyyymm = date("Y-m", strtotime('today - '.$i.' days') );
   ?>
@@ -19,7 +19,7 @@
  <?php foreach($team as $t){?>
  <tr>
  <td  class="bottom right left"><a href="/malls/daily/<?=$t['mcaNumber']?>" class="external link"><?=$t['mcaName']?></a></td>
- <?php for($i=40;$i>=0;$i--){
+ <?php for($i=0;$i<=31;$i++){
   $prevDate = date("Y-m-d", strtotime('today - '.$i.' days') );
   $yyyymm = date("Y-m", strtotime('today - '.$i.' days') );
   ?>
